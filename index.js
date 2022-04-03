4506e00168e558450dbe98c35dab7385bcfd0f14
Nevah5 <neevaah5@gmail.com>
2022-04-03 22:45:42 +0200

    Expanded code a bit

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ app.get('/', (req, res) => {
   res.send([1, 2, 3])
 })
 
+
+app.get('/response/', (req, res) => {
+  res.status(404)
+  res.send('Not found')
+})
+app.get('/user/:snowflake', (req, res) => {
+  res.send(req.params.snowflake)
+})
+
 app.listen(3000)
