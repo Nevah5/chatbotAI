62c8226cdf70b12429c071be61666050bd06a59e
Nevah5 <neevaah5@gmail.com>
2022-04-04 10:53:18 +0200

    Added check api token function into db module

--- src/db.js
+++ src/db.js
@@ -1,10 +1,20 @@
-const mysql = require('mysql');
+const mysql = require('mysql')
+require('dotenv').config()
 
 var con = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: process.env.PORT
-});
-con.connect();
+  port: process.env.DATABASE_PORT,
+  charset: 'utf8'
+})
+con.connect()
+
+exports.checkAPIToken = (token) => {
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT token FROM apiTokens WHERE token='${token}'`, (err, result) =>{
+      resolve(result.length == 1)
+    })
+  })
+}
