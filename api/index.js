eb4221a07c052d47ff8d8cb3c37ab3726f248dc5
Nevah5 <neevaah5@gmail.com>
2022-04-05 13:22:01 +0200

    Added checkTokenPromise into API

--- api/index.js
+++ api/index.js
@@ -5,20 +5,33 @@ const {API_PORT} = process.env
 
 const db = require('./src/db')
 
+//Endpoints
 app.get('/ping', (req, res) => {
   res.json({code: 200, message: "Pong!", version: process.env.API_VERSION})
 })
-
 app.get('/verify', async (req, res) => {
   const {token} = req.headers
-  if(!await db.checkAPIToken(token)) return res.status(401).json({code: 401, message: "Invalid Token."})
-  res.json({code: 200, message: "Ok!"})
+  await checkTokenPromise(token).then(response => {
+    res.status(response.code).json({code: response.code, message: response.message})
+  }).catch(response => {
+    res.status(response.code).json({code: response.code, message: response.message})
+  })
 })
-
 app.post('/train', async (req, res) => {
   const {token} = req.headers
-  if(!await db.checkAPIToken(token)) return res.status(403).json({code: 401, message: "Invalid Token."})
-  res.json({code: 200, message: "Success!"})
+  await checkTokenPromise(token).then(_ => {
+    res.status(201).json({code: 201, message: "Created!"})
+  }).catch(response =>{
+    res.status(response.code).json({code: response.code, message: response.message})
+  })
 })
+
+//Promises
+checkTokenPromise = async (token) => {
+  return new Promise(async (resolve, reject) => {
+    if(!await db.checkAPIToken(token)) return reject({code: 401, message: "Invalid Token."})
+    resolve({code: 200, message: "Ok!"})
+  })
+}
 
 app.listen(API_PORT, console.log(`API listening on port ${API_PORT}`))
