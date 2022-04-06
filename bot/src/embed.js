903076ad85ea07ef5d036ead6691abc2eac776f2
Nevah5 <neevaah5@gmail.com>
2022-04-06 10:11:41 +0200

    Removed message response embed

--- bot/src/embed.js
+++ bot/src/embed.js
@@ -37,10 +37,3 @@ exports.apiVersionChange = (data) => {
   .setColor("#3FBA54")
   .setTimestamp()
 }
-
-exports.messageResponse = msg => {
-  return new MessageEmbed()
-  .setTitle('\u200b')
-  .setDescription(msg)
-  .setColor('#FFFFFF')
-}
