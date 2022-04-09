d882e5636c2ad60ed4106b1257527db99a5f1eab
Nevah5 <neevaah5@gmail.com>
2022-04-10 00:59:24 +0200

    put broke up environment variables from process array

--- bot/index.js
+++ bot/index.js
@@ -16,16 +16,17 @@ const message = require('./src/message')
 const db = require('./src/db')
 const api = require('./src/api')
 const logger = require('./src/logger')
+const {HUB_SERVER, API_TOKEN, DISCORD_BOT_TOKEN} = process.env
 
 //check the token
-api.checkToken(process.env.API_TOKEN)
+api.checkToken(API_TOKEN)
 
 client.on('ready', async () => {
   await cache.initialize() //wait for cache to load
   logger.info(`Logged in as ${client.user.tag}!`)
 
   api.checkApi(client.user, client)
-  commands.build(client.guilds.cache.get(process.env.HUB_SERVER).commands)
+  commands.build(client.guilds.cache.get(HUB_SERVER).commands)
 })
 
 client.on('messageCreate', async msg => {
@@ -41,4 +42,4 @@ client.on('interactionCreate', interaction => {
   }
 })
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+client.login(DISCORD_BOT_TOKEN)
