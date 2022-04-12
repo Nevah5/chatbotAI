576c810fd5300feefd966fe32c93c9d6ed2dbea9
Nevah5 <neevaah5@gmail.com>
2022-04-13 00:10:06 +0200

    moved training process into function

--- api/src/ai.js
+++ api/src/ai.js
@@ -10,45 +10,38 @@ exports.start = _ => {
   return new Promise((resolve, reject) => {
     if(fs.existsSync('./data/data.json')){
       logger.info("Network already trained.")
+      //read network
       const data = fs.readFileSync('./data/data.json').toString()
       net.fromJSON(JSON.parse(data))
     }else{
       logger.info("Training Network.")
-      if(!fs.existsSync('./data/trainingdata.json')) return reject("No trainingdata.")
-      const data = JSON.stringify(fs.readFileSync('./data/trainingdata.json').toString())
-      let start = new Date()
-      pb.render(0, start)
-      net.train(data, {
-        iterations: iterations,
-        log: data => {
-          let iteration = data.split('iterations: ')[1].split(', ')[0]
-          let percentage = 100 / iterations * parseInt(iteration)
-          pb.render(percentage, start)
-        }
-      })
-      pb.render(100, start)
-      fs.writeFileSync('./data/data.json', JSON.stringify(net.toJSON()))
-      logger.info(`Training finished in ${pb.calculateReadableTime((new Date - start) / 1000)}`)
-      logger.info("Wrote network to data.json")
+      this.train()
     }
     resolve()
   })
 }
 
 exports.train = _ =>{
-  let started = new Date()
-  logger.info("Started AI training process")
-  //read trainingdata and train network
-  fs.readFileSync('trainingdata.json', (err, data) => {
-    array = data.toString().split('.')
-    net.train(array, {
-      iterations: 1,
-      log: true,
-      errorThresh: 0.01
-    }).then(_ =>{
-      fs.writeFileSync('trained.json', JSON.stringify(net.toJSON()))
+  if(!fs.existsSync('./data/trainingdata.json')) return reject("No trainingdata.")
 
-      logger.info(`Finished training AI in ${(new Date() - started) / 1000} seconds`)
-    })
+  const data = JSON.stringify(fs.readFileSync('./data/trainingdata.json').toString())
+  let start = new Date()
+
+  pb.render(0, start)
+  //train network and render pb
+  net.train(data, {
+    iterations: iterations,
+    log: data => {
+      let iteration = data.split('iterations: ')[1].split(', ')[0]
+      let percentage = 100 / iterations * parseInt(iteration)
+      pb.render(percentage, start)
+    }
   })
+  pb.render(100, start)
+  //save network
+  fs.writeFileSync('./data/data.json', JSON.stringify(net.toJSON()))
+
+  //log
+  logger.info(`Training finished in ${pb.calculateReadableTime((new Date - start) / 1000)}`)
+  logger.info("Wrote network to data.json")
 }
