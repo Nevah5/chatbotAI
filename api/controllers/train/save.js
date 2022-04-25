48fe84707363c1a1c49ba100c4d43f73fec6b6ef
Nevah5 <neevaah5@gmail.com>
2022-04-25 22:02:31 +0200

    Refactored save trainingdata

--- api/controllers/train/save.js
+++ api/controllers/train/save.js
@@ -2,8 +2,12 @@ const fs = require('fs')
 const db = require('../../utils/db')
 
 const save = async (req, res) => {
+  const trainingData = []
   db.getTrainingData().then(data => {
-    fs.writeFileSync('./data/answerdata.json', JSON.stringify(data))
+    data.forEach(field => {
+      trainingData.push({input: field.question, output: field.answer})
+    });
+    fs.writeFileSync('./data/answerdata.json', JSON.stringify(trainingData))
   })
 
   res.header("Access-Control-Allow-Origin", "*")
