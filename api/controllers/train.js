f25a1dde6e0a249365a3824a357f72bbe03a178b
Nevah5 <neevaah5@gmail.com>
2022-04-25 19:58:13 +0200

    debug

--- api/controllers/train.js
+++ api/controllers/train.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const db = require('../utils/db')
 
-const train = async (req, res) => {
+const train = (req, res) => {
   db.addTrainingData(req.headers.question, req.headers.answer, req.headers["x-forwarded-for"] || "localhost")
 
   res.status(200).json({code: 200, message: "Ok!"})
