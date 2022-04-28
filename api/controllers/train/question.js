482052d54820309aec3d798da8ffa54943d676dd
Nevah5 <neevaah5@gmail.com>
2022-04-28 09:06:16 +0200

    Changed question endpoint

--- api/controllers/train/question.js
+++ api/controllers/train/question.js
@@ -1,9 +1,11 @@
 const questions = require('../../data/questiondata.json')
+let index = 0
 
 const question = (req, res) => {
-  let randomQuestion = questions[Math.floor(Math.random() * questions.length)]
+  if(index == questions.length - 1) return res.status(423).json({code: 423, message: "Every Question answered."})
 
-  res.status(200).json({code: 200, message: "Ok!", question: randomQuestion})
+  res.status(200).json({code: 200, message: "Ok!", question: questions[index]})
+  index++
 }
 
 module.exports = question
