8689f403553340f47d56d44fe3bd46aab2cb91c4
Nevah5 <neevaah5@gmail.com>
2022-05-02 17:29:49 +0200

    Updated collector to not accept null input

--- api/data/collector.js
+++ api/data/collector.js
@@ -4,7 +4,7 @@ const prompt = require('prompt-sync')();
 arr = JSON.parse(fs.readFileSync('questiondata.json').toString())
 do{
   var i = prompt(">>> ");
-  if(i != "q") arr.push(i)
+  if(i != "q" && i !== null) arr.push(i)
 } while (i != "q");
 
 fs.writeFileSync('questiondata.json', JSON.stringify(arr))
