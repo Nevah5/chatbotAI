0cbf4a05edb456727c8bfdfd211c7b5bfaee5c41
Nevah5 <neevaah5@gmail.com>
2022-05-02 17:25:02 +0200

    Added collector script to easily add questiondata

---
+++ api/data/collector.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const prompt = require('prompt-sync')();
+
+arr = JSON.parse(fs.readFileSync('questiondata.json').toString())
+do{
+  var i = prompt(">>> ");
+  if(i != "q") arr.push(i)
+} while (i != "q");
+
+fs.writeFileSync('questiondata.json', JSON.stringify(arr))
