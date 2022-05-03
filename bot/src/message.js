b1789457504b18164b5a92b3af2c8f91464f33f7
Nevah5 <neevaah5@gmail.com>
2022-05-03 10:15:11 +0200

    Fixed bot crashing when sending empty message (with attached file or sticker)

--- bot/src/message.js
+++ bot/src/message.js
@@ -8,6 +8,7 @@ exports.sendResponse = async msg => {
   if(!api.isApiOnline() && msg.author.id === process.env.OWNER_ID) return
   if(!api.isApiOnline()) msg.delete()
   if(!api.isApiOnline()) return msg.author.send({embeds: [embed.error(cache.getCache('api-noresponse_message'))]})
+  if(msg.content === "") return msg.delete()
   await msg.channel.sendTyping()
   //move this fetch into api.js
   await fetch(process.env.API + '/response', {
