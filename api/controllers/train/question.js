0de2c8787b809026a9ed9d40c8b99c1296370070
Nevah5 <neevaah5@gmail.com>
2022-05-03 10:22:23 +0200

    Updated question endpoint to give all questions again

--- api/controllers/train/question.js
+++ api/controllers/train/question.js
@@ -2,7 +2,7 @@ const questions = require('../../data/questiondata.json')
 let index = 0
 
 const question = (req, res) => {
-  if(index == questions.length - 1) return res.status(423).json({code: 423, message: "Every Question answered."})
+  if(index >= questions.length - 1) index = 0
 
   res.status(200).json({code: 200, message: "Ok!", question: questions[index]})
   index++
