fa1c1027b5d69af1a8c7b2bd156cdf6960ae3bc8
Nevah5 <neevaah5@gmail.com>
2022-05-09 16:04:17 +0100

    Added executing ai file (chatbot.py) and trained ai (ai.db)

--- api/chatbot.py
+++ api/chatbot.py
@@ -1,8 +1,8 @@
+import sys
 import spacy
 spacy.load('en_core_web_sm')
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 
 bot = ChatBot(
     'Bot',
@@ -11,13 +11,9 @@ bot = ChatBot(
         'database_uri': 'sqlite:///ai.db'
     },
     logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
         'chatterbot.logic.BestMatch'
     ],
     read_only=True
 )
 
-print(bot.get_response('What time is it?'))
-
-print(bot.get_response('What is 7 plus 7?'))
+print(bot.get_response(sys.argv[1]))
