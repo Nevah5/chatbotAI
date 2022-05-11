8c64083cbe26a1e083f12d4b37b9be628c8012f2
Nevah5 <neevaah5@gmail.com>
2022-05-11 19:03:29 +0100

    Updated code to work with python

--- api/controllers/response.js
+++ api/controllers/response.js
@@ -1,15 +1,16 @@
-const ai = require('../utils/ai')
-const db = require('../utils/db')
+const {spawn} = require("child_process");
 const validator = require('../utils/validator')
 
 const response = async (req, res) => {
-  if(!ai.isTrained()) return res.status(500).json({code: 500, message: "Network not trained."})
-
   let input = req.headers.message
   if(!validator(input)) return res.status(400).json({code: 400, message: "Invalid Data!"})
-  ai.run(req.headers.token, input).then(msg => { //get response from ai
-    res.status(200).json({code: 200, message: "Ok!", response: msg})
-  })
+
+  var process = spawn('python3.7', ["./ai/chatbot.py", input]);
+  process.stdout.on('data', (chunk) => {
+    var textChunk = chunk.toString('utf8');
+
+    res.status(200).json({code: 200, message: "Ok!", response: textChunk})
+  });
 }
 
 module.exports = response
