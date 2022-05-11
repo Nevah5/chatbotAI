071b92b1270df904a8fa82514120e256cfdae8e4
Nevah5 <neevaah5@gmail.com>
2022-05-11 19:32:44 +0200

    Updated python file paths in test script for child_process

--- api/test.js
+++ api/test.js
@@ -1,6 +1,6 @@
 const {spawn} = require("child_process");
 
-var process = spawn('python3.7', ["chatbot.py", "hello"]);
+var process = spawn('python3.7', ["./ai/chatbot.py", "hello"]);
 
 process.stdout.on('data', (chunk) => {
   var textChunk = chunk.toString('utf8');
